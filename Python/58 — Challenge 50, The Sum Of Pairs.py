f8893724202956f747e6b1c8b632f73fcae24f28
Gustavo Augusto <gstvoaugusto@gmail.com>
2022-01-05 17:46:06 -0300

    Update 58 — Challenge 50, The Sum Of Pairs.py

---
+++ Python/58 — Challenge 50, The Sum Of Pairs.py
@@ -0,0 +1,8 @@
+sum = 0
+count = 0
+for c in range(1, 7):
+    number = int(input(f'Say the {c}° number: '))
+    if number % 2 == 0:
+        sum += number
+        count += + 1
+print(f' Total: {count} \n Sum: {sum}')
