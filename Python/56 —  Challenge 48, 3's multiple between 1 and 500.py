bd7099467b5dad968ee7a8130e921995bf800b35
Gustavo Augusto <gstvoaugusto@gmail.com>
2022-01-05 15:52:20 -0300

    2.0
    I learn a method to solve the "error"

---
+++ Python/56 —  Challenge 48, 3's multiple between 1 and 500.py
@@ -0,0 +1,13 @@
+from time import sleep
+colors = {
+    "bold": "\033[1m",
+    "clean": "\033[m",
+    "green": "\033[1;32m"
+}
+sum = 0
+count = 0
+for c in range(1, 500, 2):
+    if c % 3 == 0:
+        sum = sum + c
+        count = count + 1
+print(f"{colors['bold']}The sum of the {colors['green']}{count}{colors['clean']} {colors['bold']}numbers is:{colors['clean']} {sum}")
