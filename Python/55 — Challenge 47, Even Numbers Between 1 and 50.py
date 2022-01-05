57c6e118dc9ff6f711786c005288e619e4bbf385
Gustavo Augusto <gstvoaugusto@gmail.com>
2022-01-05 15:51:41 -0300

    Update 55 — Challenge 47, Even Numbers Between 1 and 50.py
    solving a little problem with the text

--- Python/55 — Challenge 47, Even Numbers Between 1 and 50.py
+++ Python/55 — Challenge 47, Even Numbers Between 1 and 50.py
@@ -3,6 +3,6 @@ print("This program will show the even numbers between 1 and 50.")
 sleep(1)
 for c in range(2, 51, 2):
     sleep(0.1)
-    print(c)
+    print(c, end= ' ')
 sleep(1)
-print("by Gustavo Augusto")
+print("\n by Gustavo Augusto")
