674ea65198a77dc741fcd5916df84ca6f7f3ab50
Gustavo Augusto <gstvoaugusto@gmail.com>
2022-01-08 20:35:27 -0300

    Update 01 — New.py

--- Tests/01 — New.py
+++ Tests/01 — New.py
@@ -1,6 +1,22 @@
-s = 0
-for c in range(0, 3):
-    n = int(input("Insert a number: "))
-    s += n
-print(f'The sum results is {s}!')
-print("FIM!")
+oldest = 0
+oldestname = 0
+y = 0
+qwoman = 0
+for p in range(1, 5):
+    print("----- {}º Person -----".format(p))
+    name = str(input("Name: "))
+    years = int(input("Age: "))
+    gender = str(input("Masculine/Female: ")).strip().upper()
+    y += years
+    average = (y/p)
+    if p == 1 and gender == "M":
+        oldest = years
+        oldestname = name
+    if gender == "M" and years > oldest:
+        oldest = years
+        oldestname = name
+    if gender == "F" and years < 20:
+        qwoman += 1
+print(f"Average: {average:.2f}")
+print(f"{oldestname} is the oldest man with {oldest} years")
+print(f"We have {qwoman} girls under the twenty years")
